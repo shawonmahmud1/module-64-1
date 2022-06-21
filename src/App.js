dae2c6b7ae48afe948e9b87ea5ccc9a4dc3910fb
shawonmahmud1 <alisterdavidson79@gmail.com>
2022-06-22 01:11:29 +0600

    concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    console.log(name, email);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> My own data: {users.length} </h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="email" placeholder="Email" />
+        <input type="submit" value="Submit" />
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            Name:{user.name} Email: {user.email}{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
